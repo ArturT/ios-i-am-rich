a9b5b0f557ad056c54d3654be4f164126e2ea9da
Artur Trzop <artur.trzop@lunarlogic.io>
2020-04-22 17:26:32 +0200

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Artur Trzop on 22/04/2020.
+//  Copyright © 2020 Artur Trzop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
